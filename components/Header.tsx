a603d9b35a76c6da78151665c886c3220cb54cb2
hankminden <henryminden@gmail.com>
2023-05-02 10:42:37 -0500

    Add testnet

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import Link from "next/link";
 import Button from "./Button";
 //import { Profile } from "./Profile";
-const Profile = dynamic(() => import('./Profile').then((mod) => mod.Profile), {
+const Profile = dynamic(() => import("./Profile").then((mod) => mod.Profile), {
   ssr: false,
 });
 
@@ -11,11 +11,8 @@ interface HeaderProps {
   className?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({
-  className,
-}) => {
+const Header: React.FC<HeaderProps> = ({ className }) => {
   return (
-
     <div className="flex flex-row justify-between py-8 px-8">
       <Link href="/">
         <div className="flex space-x-3 focus:cursor: cursor-pointer">
@@ -23,13 +20,12 @@ const Header: React.FC<HeaderProps> = ({
           <span className="self-center font-bookmania text-xl pl-2 md:text-2xl">
             Yacht Ordinal Exchange
           </span>
+          <span className="font-akkurat-bold text-sm">Testnet</span>
         </div>
       </Link>
 
       <Profile />
-
     </div>
-
   );
 };
 export default Header;
